7c17978524da0a6e8290296639a3047811e6cd8e
hoangnt <nguyentahoang15012003@gmail.com>
2025-06-05 14:01:37 +0700

    Initial commit: PlantCare Website Backend

---
+++ PlantCare_Website_Backend/src/main/java/com/example/PlantCare_Website_Backend/PlantCareWebsiteBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.PlantCare_Website_Backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlantCareWebsiteBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlantCareWebsiteBackendApplication.class, args);
+	}
+
+}
