17d29a0011e3920d8c5743b186d95266ca0c8849
hoangnt <nguyentahoang15012003@gmail.com>
2025-08-13 16:43:53 +0700

    fix articles imgaes

--- plantcare-backend/src/main/java/com/plantcare_backend/repository/ArticleImageRepository.java
+++ plantcare-backend/src/main/java/com/plantcare_backend/repository/ArticleImageRepository.java
@@ -1,14 +1,13 @@
 package com.plantcare_backend.repository;
 
 import com.plantcare_backend.model.ArticleImage;
+import io.lettuce.core.dynamic.annotation.Param;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
 public interface ArticleImageRepository extends JpaRepository<ArticleImage, Long> {
-    List<ArticleImage> findByArticleId(Long articleId);
-    
-    void deleteByArticleId(Long articleId);
-} 
+}
