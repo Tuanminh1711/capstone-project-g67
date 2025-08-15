3d0866643a7c647e2e0c287b0db1621b09b95b2a
hoangnt <nguyentahoang15012003@gmail.com>
2025-08-15 15:06:55 +0700

    them endpoint them anh cho new plant

--- plantcare-backend/src/main/java/com/plantcare_backend/controller/user/UserProfileController.java
+++ plantcare-backend/src/main/java/com/plantcare_backend/controller/user/UserProfileController.java
@@ -15,15 +15,12 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.Resource;
-import org.springframework.core.io.UrlResource;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.nio.file.Path;
-import java.nio.file.Paths;
 import java.util.HashMap;
 import java.util.Map;
 
